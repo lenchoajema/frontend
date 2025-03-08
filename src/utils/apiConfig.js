ece94d8274b9c09523a6442d8daf1ed4194b9431
lencho <lenchoajema@gmail.com>
2025-03-08 22:02:46 +0000

    Update API base URL and add GitHub Actions workflow for deployment

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api"; // Adjust if your backend URL differs
+const API_BASE_URL = "https://glowing-space-guacamole-965rww6px45hvjr-5000.app.github.dev/";//"http://localhost:5000/api"; // Adjust if your backend URL differs
 
 export const endpoints = {
   login: `${API_BASE_URL}/auth/login`,
